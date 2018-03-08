e15f4e994686d26da328d56d27816751e9dca9ac
Kogl1n <13698833+Kogl1n@users.noreply.github.com>
2018-03-08 22:05:23 +0100

    Update convp2.m

--- convp2.m
+++ convp2.m
@@ -4,10 +4,10 @@ function h = convp2(f, A)
 % Output: NxM-Matrix
 
 [N,M]=size(A);
-for m=1:M % Spalten
-    for n=1:N % Zeilen bzw. Eintrag von f
+for m=1:M % columns
+    for n=1:N % rows or value of f
         h(n,m)=f(1:n)*A(n:-1:1,m)+f(n+1:N)*A(N:-1:n+1,m); 
-        % hier kein Transp., da f Zeilenvektor
+        % here no transp., since f row-vector
     end
 end
 end
